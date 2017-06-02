454402083a308b451872ead4bb2faa94d7383eb9
dsenze <Tommy.vadman@dsenze.se>
2017-06-02 11:07:14 +0200

    new site

---
+++ vegan3map/js/menu.js
@@ -0,0 +1,11 @@
+$(".open-menu-button").click(function() {
+    $(".filter-map-content").show()
+    $('.responsive-menu').toggleClass('expand')
+    $('.open-menu-button').hide()
+
+});
+$(".close-menu-button").click(function() {
+    $(".filter-map-content").hide()
+    $('.open-menu-button').show()
+    $('.responsive-menu').removeClass('expand')
+});
